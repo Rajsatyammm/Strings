93b30c65a2adee1340b76a38f15b01da50901bbb
Satyam Raj <rajsatyammm@gmail.com>
2023-04-11 23:54:23 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+    public static boolean cheakPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(cheakPalindrome(str));
+    }
+}
